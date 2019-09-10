36c52ca445e5f9bb8e6c3f1bb93bbfa8c66083a7
beitangshin <55058986+beitangshin@users.noreply.github.com>
2019-09-10 23:22:50 +0200

    Add files via upload

---
+++ exercise_5.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void Function_1();
+
+char mark;
+
+void Function_1()
+{
+  int a;
+  int i=1;
+  printf("Give a number: ");
+  scanf("%d",&a);
+  do
+    {
+      printf("%d\n",i);
+      i++;
+    }
+while(i<=a);
+
+
+
+  printf("Run again(y/n)? ");
+  scanf(" %c",&mark);
+}
+int main()
+{
+  do
+    {
+      Function_1(mark);
+    }
+  while(mark=='y');
+  printf("Exiting...\n");
+
+  
+  return 0;
+
+}
